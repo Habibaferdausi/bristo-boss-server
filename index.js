3da3b7763dd85ef85014737d43688fa5bf421491
Habibaferdausi <ritu.pramanik1@gmail.com>
2023-05-29 23:47:46 +0600

    make admin

--- index.js
+++ index.js
@@ -86,6 +86,21 @@ async function run() {
       res.send(result);
     });
 
+    //admin
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
