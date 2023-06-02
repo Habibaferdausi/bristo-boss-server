c6d04d121a30582f2dfe44daf926a7326452303e
Habibaferdausi <ritu.pramanik1@gmail.com>
2023-06-02 16:44:08 +0600

    delete methord

--- index.js
+++ index.js
@@ -81,9 +81,15 @@ async function run() {
       const result = await menuCollection.find().toArray();
       res.send(result);
     });
-    app.post("/menu", verifyJWT, verifyAdmin, async (req, res) => {
+    app.post("/menu", verifyJWT, async (req, res) => {
       const newItem = req.body;
       const result = await menuCollection.insertOne(newItem);
+      res.send(result);
+    });
+    app.delete("/menu/:id", verifyJWT, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await menuCollection.deleteOne(query);
       res.send(result);
     });
 
